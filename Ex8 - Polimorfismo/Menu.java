70eabee22364a30bd140ac2472dc59bada12702c
Matheus Henrique <matheus1604.mh@gmail.com>
2022-12-05 14:57:14 +0000

    Criação da classe menu

---
+++ Ex8 - Polimorfismo/Menu.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Menu {
+    public int menu() {
+        Scanner sc = new Scanner(System.in);
+        int op;
+
+        System.out.println("BLOG: O que você quer fazer?\n");
+
+        System.out.println("(1) - Novo post de noticia.");
+        System.out.println("(2) - Nova resenha de produto.");
+        System.out.println("(3) - Novo post de outros assuntos.");
+        System.out.println("(4) - Listar todas os posts.");
+        System.out.println("(5) - Dar like no post.");
+        System.out.println("(6) - Dar dislike no post.");
+        System.out.println("(10) - Sair.");
+        System.out.print("Escolha a opção: ");
+        op = sc.nextInt();
+        System.out.println("");
+        return op;
+        
+    }
+}
